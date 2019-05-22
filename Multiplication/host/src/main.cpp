3691e97e566d8349427139946beb2f90710f75b0
guillaume <moi>
2019-05-22 20:39:35 +0200

    result in csv

--- Multiplication/host/src/main.cpp
+++ Multiplication/host/src/main.cpp
@@ -28,12 +28,14 @@ static cl_kernel kernel = NULL;
 static cl_program program = NULL;
 static cl_int status;
 
-#define DIMENSION 8192
-#define SIZE 8192
+#define DIMENSION_MAX 8192
+#define MAX_SIZE_CPU 1024
 #define LOCAL_DIM 64
+#define DEBUG 0
+
 
 float start_time, spend_time_FPGA, spend_time_CPU;
-int dimension; 
+int dim=2; 
 float* A;
 float* B;
 float* C;
@@ -55,85 +57,101 @@ void devices_info();
 
 // Entry point.
 int main() {
+  printf("MATRIX MULTIPLICATION\n");
+  printf("Matrix dimension;FPGA time;CPUTIME\n");
 
-  cl_int status;
+  while(dim <= DIMENSION_MAX)
+  {
+       if(!init()) {
+      return -1;
+    }
+    printf(" %i;",dim );
 
-  if(!init()) {
-    return -1;
+    if(DEBUG)
+      printf("Begin load some matrix\n");
+    //full matrix with not random number 
+    A=(float*)malloc(sizeof(float)*dim*dim);
+    B=(float*)malloc(sizeof(float)*dim*dim);
+    C=(float*)malloc(sizeof(float)*dim*dim);
+
+    make_matrix(A,1);
+    make_matrix(B,1);
+    make_matrix(C,0);
+    
+    if(DEBUG)
+      printf("Matrix loaded\n");
+
+    // Create the kernel using the name of the function in .cl file
+    kernel = clCreateKernel(program, "kmul2", &status);
+    //kernel = clCreateKernel(program, "kmul_opti"  , &status);
+    checkError(status, "Failed to create kernel");
+    
+
+    //FPGA Matrix Multiplication
+    start_time = getCurrentTimestamp();
+    create_buffer();
+    write_buffer();
+    setArgument();
+
+    if(DEBUG)
+      printf("Launching the kernel...\n\n");
+
+    //devices_info();
+    
+    size_t global_size[2]={(size_t)dim,(size_t)dim};
+
+    size_t local_size[2]= {(size_t)dim,(size_t)dim}; 
+    if(dim>64){
+      local_size[0]= (size_t)LOCAL_DIM;
+      local_size[1]= (size_t)LOCAL_DIM; 
+    }
+    
+    status = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, global_size, local_size, 0, NULL, NULL);
+    checkError(status, "Failed to launch kernel");
+
+    read_buffer();
+    spend_time_FPGA += getCurrentTimestamp() - start_time;
+    printf("%f;", spend_time_FPGA);
+
+    //reload the first matrix;
+    make_matrix(C,0);
+
+    //CPU Matrix multiplication
+    if(dim<=MAX_SIZE_CPU){
+      start_time =getCurrentTimestamp();
+      compute_matrix(A,B,C, dim);
+      spend_time_CPU+= getCurrentTimestamp() -start_time;
+      printf("%f;", spend_time_CPU );
+    }
+    printf("\n");
+   
+
+    status = clFinish(queue);  // Wait the queue
+    checkError(status, "Failed to finish");
+    
+    if(DEBUG)
+      printf("\nKernel execution is complete.\n");
+
+    // Free
+    cleanup(); 
+    dim=dim*2;
   }
-
-  printf("Begin load some matrix\n");
-  //full matrix with not random number 
-  A=(float*)malloc(sizeof(float)*DIMENSION*DIMENSION);
-  B=(float*)malloc(sizeof(float)*DIMENSION*DIMENSION);
-  C=(float*)malloc(sizeof(float)*DIMENSION*DIMENSION);
-
-  make_matrix(A,1);
-  make_matrix(B,1);
-  make_matrix(C,0);
-  printf("Matrix loaded\n");
-  dimension = DIMENSION;
-
-  // Create the kernel using the name of the function in .cl file
-  kernel = clCreateKernel(program, "kmul2", &status);
-  //kernel = clCreateKernel(program, "kmul_opti"  , &status);
-  checkError(status, "Failed to create kernel");
   
-
-  //FPGA Matrix Multiplication
-  start_time = getCurrentTimestamp();
-  create_buffer();
-  write_buffer();
-  setArgument();
-
-  printf("Launching the kernel...\n\n");
-
-  devices_info();
-  
-  //If kmul1
-  size_t global_size[2]={SIZE,SIZE};
-  size_t local_size[2]= {LOCAL_DIM,LOCAL_DIM}; 
-
-
-  status = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, global_size, local_size, 0, NULL, NULL);
-  checkError(status, "Failed to launch kernel");
-
-  read_buffer();
-  spend_time_FPGA += getCurrentTimestamp() - start_time;
-
-  //reload the first matrix;
-  make_matrix(C,0);
-
-  //CPU Matrix multiplication
-  start_time =getCurrentTimestamp();
-  //compute_matrix(A,B,C, dimension);
-  spend_time_CPU+= getCurrentTimestamp() -start_time;
-
-  printf("FPGA time :%f\n", spend_time_FPGA);
-  printf("CPU time%f\n", spend_time_CPU );
-
-  status = clFinish(queue);  // Wait the queue
-  checkError(status, "Failed to finish");
- 
-  printf("\nKernel execution is complete.\n");
-
-  // Free
-  cleanup();
 
   return 0;
 }
 
 void make_matrix(float* m, int type){
-  for(int i =0; i<DIMENSION; i++){
-    for(int j= 0; j<DIMENSION; j++){
+  for(int i =0; i<dim; i++){
+    for(int j= 0; j<dim; j++){
       if(type != 0){
         if(i==j){
-          m[i*DIMENSION+j]=1;
+          m[i*dim+j]=1;
         }else{
-          m[i*DIMENSION+j]=0;
+          m[i*dim+j]=0;
        }
       }else{
-        m[i*DIMENSION+j]=0;
+        m[i*dim+j]=0;
       }
     }
   }
@@ -142,10 +160,10 @@ void make_matrix(float* m, int type){
 }
 
 void show_matrix(float* m){
-  for(int i =0; i<DIMENSION; i++){
+  for(int i =0; i<dim; i++){
     printf("\n");
-    for(int j =0; j<DIMENSION; j++){
-      printf("%.1f ",m[i*DIMENSION+j]);
+    for(int j =0; j<dim; j++){
+      printf("%.1f ",m[i*dim+j]);
     }
   }
   printf("\n");
@@ -166,35 +184,40 @@ void compute_matrix(float* a, float* b ,float* c, int n){
 }
 
 void create_buffer(){
+  if(DEBUG)
+    printf("Let's create buffer\n");
 
-  printf("Let's create buffer\n");
   ABuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-          sizeof(float) * DIMENSION*DIMENSION, A, &status);
+          sizeof(float) * dim*dim, A, &status);
   BBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-          sizeof(float) * DIMENSION*DIMENSION, B, &status);
+          sizeof(float) * dim*dim, B, &status);
   CBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-          sizeof(float) * DIMENSION*DIMENSION, C, &status);
-  printf("Buffer create\n");
+          sizeof(float) * dim*dim, C, &status);
+  if(DEBUG)
+    printf("Buffer create\n");
 }
 
 void write_buffer(){
-  printf("Let's write buffer\n");
+  if(DEBUG)
+    printf("Let's write buffer\n");
+
   status  = clEnqueueWriteBuffer(queue, ABuffer, CL_FALSE,
-          0, sizeof(float) * DIMENSION*DIMENSION, A, 0, NULL, NULL);
+          0, sizeof(float) * dim*dim, A, 0, NULL, NULL);
   status  = clEnqueueWriteBuffer(queue, BBuffer, CL_FALSE,
-          0, sizeof(float) * DIMENSION*DIMENSION, B, 0, NULL, NULL);
+          0, sizeof(float) * dim*dim, B, 0, NULL, NULL);
   status  = clEnqueueWriteBuffer(queue, CBuffer, CL_FALSE,
-          0, sizeof(float) * DIMENSION*DIMENSION, C, 0, NULL, NULL);
-  printf("Buffer writen\n");
+          0, sizeof(float) * dim*dim, C, 0, NULL, NULL);
+  if(DEBUG)
+    printf("Buffer writen\n");
 }
 
 void read_buffer(){
   status  = clEnqueueReadBuffer(queue, ABuffer, CL_TRUE,
-      0, sizeof(float) * DIMENSION*DIMENSION , A  , 0, NULL, NULL);
+      0, sizeof(float) * dim*dim , A  , 0, NULL, NULL);
   status  = clEnqueueReadBuffer(queue, BBuffer, CL_TRUE,
-      0, sizeof(float) * DIMENSION*DIMENSION , B , 0, NULL, NULL);
+      0, sizeof(float) * dim*dim , B , 0, NULL, NULL);
   status  = clEnqueueReadBuffer(queue, CBuffer, CL_TRUE,
-      0, sizeof(float) * DIMENSION*DIMENSION , C  , 0, NULL, NULL);
+      0, sizeof(float) * dim*dim , C  , 0, NULL, NULL);
 
   //show_matrix(A);
   //show_matrix(B);
@@ -203,7 +226,8 @@ void read_buffer(){
 
 void setArgument (){
 
-  printf("Let's set argument\n");
+  if(DEBUG)
+    printf("Let's set argument\n");
   status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &ABuffer);
   checkError(status, "Failed to set kernel arg 0");
   
@@ -213,10 +237,10 @@ void setArgument (){
   status = clSetKernelArg(kernel, 2, sizeof(cl_mem), &CBuffer);
   checkError(status, "Failed to set kernel arg 2");
 
-  status=clSetKernelArg(kernel,3,sizeof(int), &dimension);
-  printf("Finish set argument \n");
+  status=clSetKernelArg(kernel,3,sizeof(int), &dim);
+  if(DEBUG)
+    printf("Finish set argument \n");
  
-  printf("\nKernel initialization is complete.\n");
   
 
 }
--- Multiplication/run.sh
+++ Multiplication/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-env CL_CONTEXT_EMULATOR_DEVICE_ALTERA=1 ./bin/kmul_host
+env CL_CONTEXT_EMULATOR_DEVICE_ALTERA=1 ./bin/kmul_host > result.csv
